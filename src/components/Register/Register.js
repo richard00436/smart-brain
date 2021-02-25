01c6a567b9165bc0e1b734269599134150338994
richard00436 <richard00436@gmail.com>
2021-02-25 11:54:05 +0100

    deploy smart-brain

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
 
 	onSubmitSignIn = () => {
 		//console.log(this.state);
-		fetch('http://localhost:3001/register', {
+		fetch('https://mysterious-meadow-94350.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
 
 	onSubmitSignIn = () => {
 		//console.log(this.state);
-		fetch('http://localhost:3001/signin', {
+		fetch('https://mysterious-meadow-94350.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
